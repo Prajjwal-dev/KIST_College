7ca04fae47749009139a169651a285908c6a4793
Prajjwal Maharjan <prajjwal.maharjan2013@gmail.com>
2024-09-10 18:57:35 +0545

    Index_page_start

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Toggle menu for mobile view
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+}
